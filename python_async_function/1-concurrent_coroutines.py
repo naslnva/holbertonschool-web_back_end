6fe1d82b0bffbcc676c4fd2ae2953ac4ad87174f
naslnva <13585@holbertonstudents.com>
2026-04-30 10:57:22 +0000

    Add concurrent wait_n and measure_time runtime

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -8,11 +8,12 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """Spawn wait_random n times and return sorted delays."""
+    """Spawn wait_random n times concurrently and return sorted delays."""
+    tasks = [wait_random(max_delay) for _ in range(n)]
     delays = []
 
-    for _ in range(n):
-        delay = await wait_random(max_delay)
+    for task in asyncio.as_completed(tasks):
+        delay = await task
         delays.append(delay)
 
-    return sorted(delays)
+    return delays
