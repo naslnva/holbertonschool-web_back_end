52d07c76948009a10c4be7f19b713d1b0f53c966
naslnva <13585@holbertonstudents.com>
2026-04-30 11:12:35 +0000

    Add async generator yielding random values

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -10,4 +10,4 @@ async def async_generator() -> AsyncGenerator[float, None]:
     """Yield 10 random numbers asynchronously with 1 second delay."""
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+        yield random.random() * 10
