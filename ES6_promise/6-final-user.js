79e260132092b39a410cc17231cdebf48a673593
naslnva <13585@holbertonstudents.com>
2026-07-29 21:03:17 +0000

    Complete task 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -5,8 +5,17 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]).then((results) => results.map((result) => ({
-    status: result.status,
-    value: result.value || result.reason,
-  })));
+  ]).then((results) => results.map((result) => {
+    if (result.status === 'fulfilled') {
+      return {
+        status: result.status,
+        value: result.value,
+      };
+    }
+
+    return {
+      status: result.status,
+      value: result.reason.toString(),
+    };
+  }));
 }
