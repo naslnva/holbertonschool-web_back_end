6b171890103528fbbbc5fa48c3dc0ca973ba1e57
naslnva <13585@holbertonstudents.com>
2026-08-03 16:48:36 +0000

    Complete task 8

---
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -0,0 +1,45 @@
+import readDatabase from '../utils';
+
+class StudentsController {
+  static getAllStudents(req, res) {
+    const database = process.argv[2];
+
+    readDatabase(database)
+      .then((fields) => {
+        const response = ['This is the list of our students'];
+
+        Object.keys(fields)
+          .sort((a, b) => a.localeCompare(b))
+          .forEach((field) => {
+            response.push(
+              `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`,
+            );
+          });
+
+        res.status(200).send(response.join('\n'));
+      })
+      .catch(() => {
+        res.status(500).send('Cannot load the database');
+      });
+  }
+
+  static getAllStudentsByMajor(req, res) {
+    const database = process.argv[2];
+    const { major } = req.params;
+
+    if (major !== 'CS' && major !== 'SWE') {
+      res.status(500).send('Major parameter must be CS or SWE');
+      return;
+    }
+
+    readDatabase(database)
+      .then((fields) => {
+        res.status(200).send(`List: ${fields[major].join(', ')}`);
+      })
+      .catch(() => {
+        res.status(500).send('Cannot load the database');
+      });
+  }
+}
+
+export default StudentsController;
---
+++ Node_JS_basic/full_server/utils.js
@@ -0,0 +1,31 @@
+import fs from 'fs';
+
+const readDatabase = (path) => new Promise((resolve, reject) => {
+  fs.readFile(path, 'utf8', (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+      return;
+    }
+
+    const lines = data
+      .split('\n')
+      .filter((line) => line.trim() !== '');
+
+    const students = lines.slice(1);
+    const fields = {};
+
+    students.forEach((student) => {
+      const [firstname, , , field] = student.split(',');
+
+      if (!fields[field]) {
+        fields[field] = [];
+      }
+
+      fields[field].push(firstname);
+    });
+
+    resolve(fields);
+  });
+});
+
+export default readDatabase;
